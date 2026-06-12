be874a9fc091c82432380e86f88283b5e2e93692
Dawud354 <dawud.a.hussain@gmail.com>
2026-06-12 15:15:07 +0100

    Feat: Implement CommandQueue
    This is used to act as a bridge between input and logic

---
+++ CommandQueue.cpp
@@ -0,0 +1,22 @@
+//
+// Created by dawud on 6/12/26.
+//
+
+#include "CommandQueue.h"
+
+void CommandQueue::push(const Command& command)
+{
+    mQueue.push(command);
+}
+
+Command CommandQueue::pop()
+{
+    Command command = mQueue.front();
+    mQueue.pop();
+    return command;
+}
+
+bool CommandQueue::isEmpty() const
+{
+    return mQueue.empty();
+}
---
+++ CommandQueue.h
@@ -0,0 +1,29 @@
+//
+// Created by dawud on 6/12/26.
+//
+
+#ifndef DAWUDENGINE_COMMANDQUEUE_H
+#define DAWUDENGINE_COMMANDQUEUE_H
+
+#include "Command.h"
+
+#include <queue>
+
+
+// A thin wrapper class
+// The class acts as a bridge betwene input an
+// The methods just call the underlying C++ Queue
+class CommandQueue
+{
+public:
+    void						push(const Command& command);
+    Command						pop();
+    bool						isEmpty() const;
+
+
+private:
+    std::queue<Command>			mQueue;
+};
+
+
+#endif //DAWUDENGINE_COMMANDQUEUE_H
