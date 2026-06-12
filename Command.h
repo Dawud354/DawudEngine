6cb4d3f13b8fc7173a8b4e33e6894b8e619c4a51
Dawud354 <dawud.a.hussain@gmail.com>
2026-06-12 17:04:12 +0100

    Feat: Add derivedAction function to Command.h

--- Command.h
+++ Command.h
@@ -21,5 +21,19 @@ struct Command
     unsigned int								category;
 };
 
+// This is used to avoid having to downcast each time we need to work on a child of SceneNode
+// Bit ugly bu
+template <typename GameObject, typename Function>
+std::function<void(SceneNode&, sf::Time)> derivedAction(Function fn)
+{
+    return [=] (SceneNode& node, sf::Time dt)
+    {
+        // Check if cast is safe
+        assert(dynamic_cast<GameObject*>(&node) != nullptr);
+
+        // Downcast node and invoke function on it
+        fn(static_cast<GameObject&>(node), dt);
+    };
+}
 
 #endif //DAWUDENGINE_COMMAND_H
