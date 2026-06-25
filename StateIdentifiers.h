a09dc211f4efb38121d52ccf4b177d1a8ab5ab11
Dawud354 <dawud.a.hussain@gmail.com>
2026-06-25 16:44:02 +0100

    Feat: Add StateIdentifiers Class
    This is needed to state class

---
+++ StateIdentifiers.h
@@ -0,0 +1,21 @@
+//
+// Created by dawud on 6/25/26.
+//
+
+#ifndef DAWUDENGINE_STATEIDENTIFIERS_H
+#define DAWUDENGINE_STATEIDENTIFIERS_H
+
+namespace States
+{
+    enum ID
+    {
+        None,
+        Title,
+        Menu,
+        Game,
+        Loading,
+        Pause
+    };
+}
+
+#endif //DAWUDENGINE_STATEIDENTIFIERS_H
