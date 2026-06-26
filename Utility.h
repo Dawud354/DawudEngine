cda984e9f7567742b19fa209e8f124fbd1fe28f5
Dawud354 <dawud.a.hussain@gmail.com>
2026-06-26 17:10:23 +0100

    Feat: Add Utility class

---
+++ Utility.h
@@ -0,0 +1,18 @@
+//
+// Created by dawud on 6/26/26.
+//
+
+#ifndef DAWUDENGINE_UTILITY_H
+#define DAWUDENGINE_UTILITY_H
+
+namespace sf
+{
+    class Sprite;
+    class Text;
+}
+
+void centerOrigin(sf::Sprite& sprite);
+void centerOrigin(sf::Text& text);
+
+
+#endif //DAWUDENGINE_UTILITY_H
