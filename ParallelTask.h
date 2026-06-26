8ed5ce42594501eaa66c319481a675574967916a
Dawud354 <dawud.a.hussain@gmail.com>
2026-06-26 18:04:07 +0100

    Feat: Implement ParallelTask class

---
+++ ParallelTask.h
@@ -0,0 +1,32 @@
+//
+// Created by dawud on 6/26/26.
+//
+
+#ifndef DAWUDENGINE_PARALLELTASK_H
+#define DAWUDENGINE_PARALLELTASK_H
+
+
+#include <SFML/System/Thread.hpp>
+#include <SFML/System/Mutex.hpp>
+#include <SFML/System/Lock.hpp>
+#include <SFML/System/Clock.hpp>
+
+
+class ParallelTask
+{
+public:
+    ParallelTask();
+    void				execute();
+    bool				isFinished();
+    float				getCompletion();
+
+private:
+    void				runTask();
+    
+    sf::Thread			mThread;
+    bool				mFinished;
+    sf::Clock			mElapsedTime;
+    sf::Mutex			mMutex;
+};
+
+#endif //DAWUDENGINE_PARALLELTASK_H
