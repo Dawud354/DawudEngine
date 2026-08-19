5a04f7b8e3b44808a312481c6b4e798519995d61
Dawud354 <dawud.a.hussain@gmail.com>
2026-08-19 09:00:12 +0100

    Feat: Add collision code to SceneNode.h

--- include/DawudEngine/Scene/SceneNode.h
+++ include/DawudEngine/Scene/SceneNode.h
@@ -5,13 +5,16 @@
 #ifndef DAWUDENGINE_SCENENODE_H
 #define DAWUDENGINE_SCENENODE_H
 #include <memory>
+#include <set>
 #include <SFML/Graphics/Transformable.hpp>
 #include <SFML/Graphics/Drawable.hpp>
 #include <SFML/System/Time.hpp>
 
 #include <vector>
 
-// forward decleration
+#include "Category.h"
+
+// forward declaration
 struct Command;
 /*
  * This class is used make our tree structure to store game stuff that is to be drawn
@@ -28,6 +31,7 @@ class SceneNode:  public sf::Transformable , public sf::Drawable{
 
         // The book originally used a typedef, but I learnt that using is the better new way to do it so I used that instead
         using UniquePtr = std::unique_ptr<SceneNode>;
+        using Pair = std::pair<SceneNode*, SceneNode*>;
 
         SceneNode();
         void attachChild(UniquePtr child);
@@ -41,6 +45,15 @@ class SceneNode:  public sf::Transformable , public sf::Drawable{
         virtual unsigned int getCategory() const;
         void onCommand(const Command& command, sf::Time dt);
 
+        void checkSceneCollision(SceneNode& sceneGraph, std::set<Pair>& collisionPairs);
+        void checkNodeCollision(SceneNode& node, std::set<Pair>& collisionPairs);
+        void removeWrecks();
+        virtual sf::FloatRect getBoundingRect() const;
+        virtual bool isMarkedForRemoval() const;
+        virtual bool isDestroyed() const;
+
+
+
     private:
 
         virtual void updateCurrent(sf::Time dt);
@@ -50,16 +63,20 @@ class SceneNode:  public sf::Transformable , public sf::Drawable{
         void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
         virtual void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const;
         void drawChildren(sf::RenderTarget& target, sf::RenderStates states) const;
-
+        // used for debugging purposes
+        void drawBoundingRect(sf::RenderTarget& target, sf::RenderStates states) const;
 
         std::vector<UniquePtr> children;
         // used to traverse up the graph
         // fine to be a raw pointer as the way our code is it would be very hard
         // to have a dangling pointer
         SceneNode* parent;
+        Category::Type defaultCategory;
 
 
 };
 
+bool collision(const SceneNode& node1, const SceneNode& node2);
+float distance(const SceneNode& node1, const SceneNode& node2);
 
 #endif //DAWUDENGINE_SCENENODE_H
