c75cea631a30f8c435f7b01f8d51d4eddc5417fd
Dawud354 <dawud.a.hussain@gmail.com>
2026-08-20 08:48:21 +0100

    Comment: Entity.h now up to chapter 7
    I added the comment so, I would remember if I finished the chapter 7
    changes.

--- include/DawudEngine/Scene/Entity.h
+++ include/DawudEngine/Scene/Entity.h
@@ -1,27 +1,40 @@
 //
 // Created by dawud on 6/5/26.
-//
+// Updated to chapter 7
 
 #ifndef DAWUDENGINE_ENTITY_H
 #define DAWUDENGINE_ENTITY_H
 
 #include "SceneNode.h"
+#include "CommandQueue.h"
 
 class Entity : public SceneNode
 {
 public:
+    explicit Entity(int hitpoints);
+
     void setVelocity(const sf::Vector2f& newVelocity);
     void setVelocity(float vx, float vy);
     sf::Vector2f getVelocity() const;
     void accelerate(sf::Vector2f acceleration);
     void accelerate(float vx, float vy);
 
+    int getHitpoints() const;
+    void repair(int points);
+    void damage(int points);
+    void destroy();
+    bool isDestroyed() const override;
+
+protected:
+    virtual void updateCurrent(sf::Time dt, CommandQueue& commands);
+
 
 private:
-    void updateCurrent(sf::Time dt) override;
+    void updateCurrent(sf::Time dt);
 
     // The SFML book made every entity have a velocity. It was just a design choice
     sf::Vector2f velocity;
+    int hp;
 };
 
 
