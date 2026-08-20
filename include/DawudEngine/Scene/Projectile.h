8acf75bb3445416b4385987e88659f66ddf9b25e
Dawud354 <dawud.a.hussain@gmail.com>
2026-08-20 16:06:24 +0100

    Feat: Add Projectile class .cpp and .h
    The .cpp has not been coded. Clion generated it automatically.

---
+++ include/DawudEngine/Scene/Projectile.h
@@ -0,0 +1,48 @@
+//
+// Created by dawud on 8/20/26.
+//
+
+#ifndef DAWUDENGINE_PROJECTILE_H
+#define DAWUDENGINE_PROJECTILE_H
+#include <SFML/Graphics/Sprite.hpp>
+
+#include "Entity.h"
+#include "ResourceIndentifiers.h"
+
+
+class Projectile : public Entity
+{
+public:
+    enum class Type
+    {
+        AlliedBullet,
+        EnemyBullet,
+        Missile,
+        TypeCount
+    };
+
+    Projectile(Type type, const TextureHolder& textures);
+
+    void guideTowards(sf::Vector2f);
+    bool isGuided() const;
+
+    unsigned int getCategory() const override;
+    sf::FloatRect getBoundingRect() const override;
+    float getMaxSpeed();
+    int getDamage() const;
+
+protected:
+    void updateCurrent(sf::Time dt, CommandQueue &commands) override;
+
+
+private:
+    void drawCurrent(sf::RenderTarget &target, sf::RenderStates states) const override;
+
+
+    Type type;
+    sf::Sprite sprite;
+    sf::Vector2f targetDirection;
+};
+
+
+#endif //DAWUDENGINE_PROJECTILE_H
---
+++ src/Scene/Projectile.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dawud on 8/20/26.
+//
+
+#include "../../include/DawudEngine/Scene/Projectile.h"
