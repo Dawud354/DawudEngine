03f61809cfaa8ae64e82f15a1d51bcd978d3f2eb
Dawud354 <dawud.a.hussain@gmail.com>
2026-08-21 17:52:18 +0100

    Feat: Create TextNode class
    Again the cpp has not been implemented

---
+++ include/DawudEngine/Scene/TextNode.h
@@ -0,0 +1,29 @@
+//
+// Created by dawud on 8/21/26.
+//
+
+#ifndef DAWUDENGINE_TEXTNODE_H
+#define DAWUDENGINE_TEXTNODE_H
+#include <SFML/Graphics/Text.hpp>
+
+#include "ResourceIndentifiers.h"
+#include "SceneNode.h"
+
+
+class TextNode : public SceneNode
+{
+public:
+    explicit TextNode(const FontHolder& fonts, const std::string& text);
+
+    void setString(const std::string& text);
+
+protected:
+    void drawCurrent(sf::RenderTarget &target, sf::RenderStates states) const override;
+
+private:
+    sf::Text text;
+
+};
+
+
+#endif //DAWUDENGINE_TEXTNODE_H
---
+++ src/Scene/TextNode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dawud on 8/21/26.
+//
+
+#include "../../include/DawudEngine/Scene/TextNode.h"
