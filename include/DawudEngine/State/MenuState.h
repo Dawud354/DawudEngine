7ac0d5e14a454b3b9dffbc452e519e4da7b95b21
Dawud354 <dawud.a.hussain@gmail.com>
2026-08-24 15:02:47 +0100

    Update: Add Container to MenuState.h
    Removed the hardcoded way as before I did not have the full GUI suite.

--- include/DawudEngine/State/MenuState.h
+++ include/DawudEngine/State/MenuState.h
@@ -5,34 +5,24 @@
 #ifndef DAWUDENGINE_MENUSTATE_H
 #define DAWUDENGINE_MENUSTATE_H
 
-#include "../State.h"
-
 #include <SFML/Graphics/Sprite.hpp>
-#include <SFML/Graphics/Text.hpp>
+
+#include "State.h"
+#include "GUI/Container.h"
 
 class MenuState : public State {
 public:
     MenuState(StateStack &stack, Context context);
 
-    virtual void draw();
-
-    virtual bool update(sf::Time dt);
-
-    virtual bool handleEvent(const sf::Event &event);
+    void draw() override;
+    bool update(sf::Time dt) override;
+    bool handleEvent(const sf::Event &event) override;
 
     void updateOptionText();
 
 private:
-    enum OptionNames {
-        Play,
-        Exit,
-    };
-
-private:
     sf::Sprite backgroundSprite;
-
-    std::vector<sf::Text> options;
-    std::size_t optionIndex;
+    GUI::Container GUIContainer;
 };
 
 
