769994e6a0f23c34b34d4a2eb81c879ced8b677c
Dawud354 <dawud.a.hussain@gmail.com>
2026-08-24 11:54:05 +0100

    Feat: Create DataTables for the system
    Again cpp has not been implemented. The data here is for the nodes, but
    I felt that it would be better in Core.

---
+++ include/DawudEngine/Core/DataTables.h
@@ -0,0 +1,53 @@
+//
+// Created by dawud on 8/24/26.
+//
+
+#ifndef DAWUDENGINE_DATATABLES_H
+#define DAWUDENGINE_DATATABLES_H
+
+
+#include <SFML/System/Time.hpp>
+
+#include <vector>
+#include <functional>
+
+#include "ResourceIndentifiers.h"
+
+class Aircraft;
+
+struct Direction {
+    Direction(float angle, float distance)
+        : angle(angle)
+        , distance(distance) {
+    }
+
+    float angle;
+    float distance;
+};
+
+struct AircraftData {
+    int hitpoints;
+    float speed;
+    Textures::ID texture;
+    sf::Time fireInterval;
+    std::vector<Direction> directions;
+};
+
+struct ProjectileData {
+    int damage;
+    float speed;
+    Textures::ID texture;
+};
+
+struct PickupData {
+    std::function<void(Aircraft &)> action;
+    Textures::ID texture;
+};
+
+
+std::vector<AircraftData> initializeAircraftData();
+std::vector<ProjectileData> initializeProjectileData();
+std::vector<PickupData> initializePickupData();
+
+
+#endif //DAWUDENGINE_DATATABLES_H
