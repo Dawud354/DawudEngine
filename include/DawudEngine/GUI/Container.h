de31a2dbbef1a1bfa0f3c0e3af5926e2fcc2dcdd
Dawud354 <dawud.a.hussain@gmail.com>
2026-08-24 11:22:08 +0100

    Feat: Create Container class for the GUI
    Again cpp has not been implemented

---
+++ include/DawudEngine/GUI/Container.h
@@ -0,0 +1,39 @@
+//
+// Created by dawud on 8/24/26.
+//
+
+#ifndef DAWUDENGINE_CONTAINER_H
+#define DAWUDENGINE_CONTAINER_H
+
+
+#include <vector>
+#include <memory>
+
+#include "Component.h"
+
+namespace GUI {
+    class Container : public Component {
+    public:
+        using SharedPtr =  std::shared_ptr<Container>;
+
+        Container();
+
+        void pack(Component::SharedPtr component);
+
+        bool isSelectable() const override;
+        void handleEvent(const sf::Event &event) override;
+
+    private:
+        void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
+
+        bool hasSelection() const;
+        void select(std::size_t index);
+        void selectNext();
+        void selectPrevious();
+
+        std::vector<Component::SharedPtr> children;
+        int selectedChild;
+    };
+}
+
+#endif //DAWUDENGINE_CONTAINER_H
