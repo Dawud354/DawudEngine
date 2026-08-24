ea8cd14d94721eeeb5e62cc7970a1ff8bea778ef
Dawud354 <dawud.a.hussain@gmail.com>
2026-08-24 15:08:24 +0100

    Feat: Create Pickup for the game
    Again cpp has not been implemented.

---
+++ include/DawudEngine/Scene/Pickup.h
@@ -0,0 +1,42 @@
+//
+// Created by dawud on 8/24/26.
+//
+
+#ifndef DAWUDENGINE_PICKUP_H
+#define DAWUDENGINE_PICKUP_H
+
+#include "Entity.h"
+#include "ResourceIndentifiers.h"
+
+#include <SFML/Graphics/Sprite.hpp>
+
+
+class Aircraft;
+
+class Pickup : public Entity {
+public:
+    enum Type {
+        HealthRefill,
+        MissileRefill,
+        FireSpread,
+        FireRate,
+        TypeCount
+    };
+
+    Pickup(Type type, const TextureHolder &textures);
+
+    unsigned int getCategory() const override;
+    sf::FloatRect getBoundingRect() const override;
+
+    void apply(Aircraft &player) const;
+
+protected:
+    void drawCurrent(sf::RenderTarget &target, sf::RenderStates states) const override;
+
+private:
+    Type type;
+    sf::Sprite sprite;
+};
+
+
+#endif //DAWUDENGINE_PICKUP_H
