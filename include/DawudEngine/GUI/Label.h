6acd9136dd6c218ea9e40675e74f59e1a882e896
Dawud354 <dawud.a.hussain@gmail.com>
2026-08-24 15:00:07 +0100

    Feat: Create Label for the GUI
    Again cpp has not been implemented.

---
+++ include/DawudEngine/GUI/Label.h
@@ -0,0 +1,33 @@
+//
+// Created by dawud on 8/24/26.
+//
+
+#ifndef DAWUDENGINE_LABEL_H
+#define DAWUDENGINE_LABEL_H
+
+#include <SFML/Graphics/Text.hpp>
+
+#include "Component.h"
+#include "ResourceIndentifiers.h"
+#include "ResourceHolder.h"
+
+namespace GUI {
+    class Label : public Component {
+    public:
+        using SharedPtr =  std::shared_ptr<Label>;
+
+        Label(const std::string &text, const FontHolder &fonts);
+
+        bool isSelectable() const override;
+        void setText(const std::string &text);
+
+        void handleEvent(const sf::Event &event) override;
+
+    private:
+        void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
+
+        sf::Text text;
+    };
+}
+
+#endif //DAWUDENGINE_LABEL_H
---
+++ src/GUI/Label.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dawud on 8/24/26.
+//
+
+#include "Label.h"
