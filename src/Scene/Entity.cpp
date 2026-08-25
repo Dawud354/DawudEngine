ef0c1bc1a768fb286ede59191c17435e6dac75f1
Dawud354 <dawud.a.hussain@gmail.com>
2026-08-25 17:38:02 +0100

    Feat: Update Entity.cpp to chapter 7
    This was all the hp stuff

--- src/Scene/Entity.cpp
+++ src/Scene/Entity.cpp
@@ -2,7 +2,15 @@
 // Created by dawud on 6/5/26.
 //
 
+#include <cassert>
+
 #include "Entity.h"
+
+Entity::Entity(int hitpoints)
+    :velocity()
+    , hp(hitpoints) {
+
+}
 
 void Entity::setVelocity(const sf::Vector2f& newVelocity) {
     velocity = newVelocity;
@@ -33,4 +41,32 @@ void Entity::accelerate(float vx, float vy)
 {
     velocity.x += vx;
     velocity.y += vy;
+}
+
+int Entity::getHitpoints() const {
+    return hp;
+}
+
+void Entity::repair(int points) {
+    assert(points > 0);
+
+    hp += points;
+}
+
+void Entity::damage(int points) {
+    assert(points > 0);
+
+    hp -= points;
+}
+
+void Entity::destroy() {
+    hp = 0;
+}
+
+bool Entity::isDestroyed() const {
+    return hp <= 0;
+}
+
+void Entity::updateCurrent(sf::Time dt, CommandQueue &commands) {
+    move(velocity * dt.asSeconds());
 }
