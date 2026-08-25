5e361ff90e510450916bf5d6c8e803d192c26572
Dawud354 <dawud.a.hussain@gmail.com>
2026-08-25 11:22:13 +0100

    Feat: Update Application.cpp to chapter 7

--- src/Core/Application.cpp
+++ src/Core/Application.cpp
@@ -4,21 +4,52 @@
 //
 
 #include "Application.h"
-#include "ResourceHolder.h"
 
 #include <iostream>
+
+#include "GameOverState.h"
+#include "Utility.h"
+#include "State/GameState.h"
+#include "State/MenuState.h"
+#include "State/PauseState.h"
+#include "State/SettingsState.h"
+#include "State/State.h"
+#include "State/TitleState.h"
 
 
 // set the constants here
 // not sure why here but the SFMl game dev book did it here
-const float Application::PlayerSpeed = 100.f;
 const sf::Time Application::TimePerFrame = sf::seconds(1.f/60.f);
 
 
 Application::Application()
-: mainWindow(sf::VideoMode(640, 480), "SFML Application")
-, world(mainWindow)
+: mainWindow(sf::VideoMode(1024, 768), "SFML Application", sf::Style::Close)
+, textures()
+, fonts()
+, player()
+, stateStack(State::Context(mainWindow, textures, fonts, player))
+, statisticsText()
+, statisticsUpdateTime()
+, statisticsNumFrames(0)
 {
+    //I think this is to stop me spamming fire
+    mainWindow. setKeyRepeatEnabled(false);
+
+    fonts.load(Fonts::Main, "Media/Sansation.tff");
+
+    textures.load(Textures::TitleScreen, "Media/Textures/TitleScreen.png");
+    textures.load(Textures::ButtonNormal,"Media/Textures/ButtonNormal.png");
+    textures.load(Textures::ButtonSelected,"Media/Textures/ButtonSelected.png");
+    textures.load(Textures::ButtonPressed,"Media/Textures/ButtonPressed.png");
+
+    statisticsText.setFont(fonts.get(Fonts::Main));
+    statisticsText.setPosition(5.f, 5.f);
+    statisticsText.setCharacterSize(10u);
+
+    registerStates();
+    stateStack.pushState(States::Title);
+
+
 }
 
 /*
@@ -31,56 +62,68 @@ void Application::run()
 
     // this line is to make the update logic update at a fixed interval to avoid issues with B
     sf::Time timeSinceLastUpdate = sf::Time::Zero;
+
     while (mainWindow.isOpen())
     {
-        // this outer call is to help with responsiveness
-        // say we pressed the close button if we only had the inner one it would take 16.6ms to
-        // respond which can feel slow to a user
-        // ---------------------------------------
-        // in Chapter 3 the book seemed to remove it but I will keep it commented for now
-        //processEvents();
-        timeSinceLastUpdate += clock.restart();
 
+        const sf::Time dt = clock.restart();
+        timeSinceLastUpdate += dt;
         while (timeSinceLastUpdate > TimePerFrame) {
             timeSinceLastUpdate -= TimePerFrame;
-            processEvents();
+            processInput();
             update(TimePerFrame);
+
+            // check inside this loop cos stack might be empty before update() call
+            if (stateStack.isEmpty()) {
+                mainWindow.close();
+            }
         }
+        updateStatistics(dt);
         render();
     }
 }
 
-void Application::processEvents() {
+void Application::processInput() {
     sf::Event e;
     while (mainWindow.pollEvent(e)) {
-        switch (e.type) {
-            case sf::Event::KeyPressed:
-                handlePlayerInput(e.key.code, true);
-                break;
-            case sf::Event::KeyReleased:
-                handlePlayerInput(e.key.code, false);
-                break;
-            case sf::Event::Closed:
-                mainWindow.close();
-                break;
-            default:
-                break;
-        }
+        stateStack.handleEvent(e);
+
+        if (e.type == sf::Event::Closed)
+            mainWindow.close();
     }
 }
 
 void Application::update(sf::Time dt){
-    world.update(dt);
+    stateStack.update(dt);
 }
 
 void Application::render() {
     mainWindow.clear();
-    world.draw();
+
+    stateStack.draw();
 
 	mainWindow.setView(mainWindow.getDefaultView());
+    mainWindow.draw(statisticsText);
+
     mainWindow.display();
 }
 
-void Application::handlePlayerInput(sf::Keyboard::Key key, bool isPressed) {
+void Application::updateStatistics(sf::Time dt) {
+    statisticsUpdateTime += dt;
+    statisticsNumFrames += 1;
+    if (statisticsUpdateTime > sf::seconds(1.0f)) {
+        statisticsText.setString("FPS: " + std::to_string((statisticsNumFrames)));
+
+        statisticsUpdateTime -= sf::seconds(1.0f);
+        statisticsNumFrames = 0;
+    }
+}
+void Application::registerStates() {
+    stateStack.registerState<TitleState>(States::Title);
+    stateStack.registerState<MenuState>(States::Menu);
+    stateStack.registerState<GameState>(States::Game);
+    stateStack.registerState<PauseState>(States::Pause);
+    stateStack.registerState<SettingsState>(States::Settings);
+    stateStack.registerState<GameOverState>(States::GameOver);
 }
 
