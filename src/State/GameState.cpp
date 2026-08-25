b9abfb2a219a765175ffa60a370c89e10754510e
Dawud354 <dawud.a.hussain@gmail.com>
2026-08-25 17:55:14 +0100

    Feat: Update GameState.cpp to chapter 7

--- src/State/GameState.cpp
+++ src/State/GameState.cpp
@@ -2,23 +2,32 @@
 // Created by dawud on 6/26/26.
 //
 
-#include "GameState.h"
+#include "State/GameState.h"
 
 GameState::GameState(StateStack& stack, Context context)
 : State(stack, context)
 , world(*context.window)
 , player(*context.player)
 {
+    player.setMissionStatus(PlayerInputHandler::MissionStatus::MissionRunning);
 }
 
 void GameState::draw()
 {
     world.draw();
 }
-
+// clion was being weird and didn't recognised requestStatePush as I was typing
 bool GameState::update(sf::Time dt)
 {
     world.update(dt);
+
+    if (!world.hasAlivePlayer()) {
+        player.setMissionStatus(PlayerInputHandler::MissionStatus::MissionFailure);
+        requestStackPush(States::GameOver);
+    } else if (world.hasPlayerReachedEnd()) {
+        player.setMissionStatus(PlayerInputHandler::MissionStatus::MissionSuccess);
+        requestStackPush(States::GameOver);
+    }
 
     CommandQueue& commands = world.getCommandQueue();
     player.handleRealTimeInput(commands);
