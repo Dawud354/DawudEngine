23fb0a4970b34b4df8f20660c6bf25433897da7e
Dawud354 <dawud.a.hussain@gmail.com>
2026-08-25 15:39:20 +0100

    Feat: Update Component.cpp to chapter 7

--- src/GUI/Component.cpp
+++ src/GUI/Component.cpp
@@ -2,4 +2,41 @@
 // Created by dawud on 8/23/26.
 //
 
-#include "../../include/DawudEngine/GUI/Component.h"
+// a cool class. it seems basic but it somehow can be so useful
+
+#include "GUI/Component.h"
+
+namespace GUI {
+    Component::Component()
+        : mIsSelected(false)
+          , mIsActive(false) {
+    }
+
+    Component::~Component() = default;
+
+    bool Component::isSelected() const {
+        return mIsSelected;
+    }
+
+    void Component::select() {
+        mIsSelected = true;
+    }
+
+    void Component::deselect() {
+        mIsSelected = false;
+    }
+
+    bool Component::isActive() const {
+        return mIsActive;
+    }
+
+    void Component::activate() {
+        mIsActive = true;
+    }
+
+    void Component::deactivate() {
+        mIsActive = false;
+    }
+}
+
+}
